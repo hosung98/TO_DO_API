264d0bb0069ebbd99563c37c3a21fe214eea719a
kimhosung <70985410+hosung1998@users.noreply.github.com>
2023-02-06 19:10:29 +0900

    login 화면 mybatis 로 변환

--- DBconnection.js
+++ DBconnection.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql');  // mysql 모듈 로드
+const mybatisMapper = require('mybatis-mapper');
+
 const conn = {  // mysql 접속 설정
     host: '43.201.121.190',
     port: '51211',
@@ -10,17 +12,26 @@ const conn = {  // mysql 접속 설정
 
 let connection = mysql.createConnection(conn); // DB 커넥션 생성
 connection.connect();   // DB 접속
- 
 
 
-sql = "SELECT * FROM T_IFO_MEMBERS";
+mybatisMapper.createMapper(['./mybatis-mapper/login.xml']);
  
-connection.query(sql, function (err, results, fields) { 
-    if (err) {
-        console.log(err);
+let param = {
+    id : "admin1"
+}
+
+l//질의문 형식
+let format = {language: 'sql', indent: '  '};
+let query = mybatisMapper.getStatement('testMapper', 'testBasic', param, format);
+//첫번째는 xml의 name값, 두번째는 해당 xml의 id값, 세번째는 파라미터, 마지막은 포맷 입니다.
+
+console.log(query);  //해당쿼리가 조합된 것을 볼 수 있습니다.
+
+connection.connect();
+connection.query(query, function (error, results, fields) {  //조회
+    if (error) {
+        console.log(error);
     }
     console.log(results);
 });
- 
- 
-connection.end(); // DB 접속 종료
+connection.end();
