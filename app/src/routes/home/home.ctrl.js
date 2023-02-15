2bb4b7a88b414f0ab831b850033aa1258c945043
cosdev40 <cosdev40@cosmo.cup>
2023-02-15 10:27:09 +0900

    jwt 토큰생성

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,4 +1,7 @@
 "use strict";
+const jwt = require('jsonwebtoken');
+const secretKey = 'mySecretKey';
+
 
 const process = {
   login: (req, res) => {
@@ -22,17 +25,27 @@ const process = {
 
     conn.query(query, function (error, results, fields) {  
         //Error
-        console.log("error ===> " + results[0].cnt);
         if (results[0].cnt == 0) {
             result.success = false;
             result.msg = "해당 로그인 정보가 없습니다.";
             res.status(400).json(result);
             return;
         }
+        //Success
         
+        //jwtToken 생성
+        const payload = {
+          id: results.id,
+          password: results.password
+        };
+        const options = {
+          expiresIn: '1h'
+        };
+        const token = jwt.sign(payload, secretKey, options);
+        console.log(token);
+
         result.success = true;
         result.msg = "";      
-        
         res.json(result);
     });
   },
