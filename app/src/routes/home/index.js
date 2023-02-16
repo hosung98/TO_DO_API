c0d8c48e2e4cd95041e0aa8131309d345af6fb22
kimhosung <70985410+hosung1998@users.noreply.github.com>
2023-02-16 20:41:53 +0900

    addboard 추가

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,6 +8,7 @@ let ctrl = require("./home.ctrl");
 let ctrlRegister = require("./register.ctrl");
 let ctrlMyPage = require("./mypage.ctrl");
 let ctrlContent = require("./findContent.ctrl.");
+let ctrlBoard = require("./addBoard.ctrl");
 
 //view
 router.get("/", ctrl.show.home);
@@ -22,6 +23,7 @@ router.get("/register", ctrlRegister.check.idCheck);
 //control(POST 방식)
 router.post("/register", ctrlRegister.sign.register);
 router.post("/login", ctrl.process.login); 
+router.post("/addBoard", ctrlBoard.addBoardCtrl.addBoard); 
 
 // 외부로 내보내기
 module.exports = router;
